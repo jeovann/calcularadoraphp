dd4d65b76b39b23b089eee1de4ead834206a596f
jeovan <jeovann@hotmail.com>
2018-03-22 18:53:37 -0400

    first commit

---
+++ calculadora.php
@@ -0,0 +1,20 @@
+<HTML>
+<HEAD>
+<title>CALCULADORA</title>
+</HEAD>
+<BODY>
+<form method="post" action="calcular.php">
+Valor 1
+<input type="text" name="valor1" size="5" />
+<select name="tipo">
+<option selected="selected"> soma</option>
+<option>Subitrair</option>
+<option>Multiplicar</optin>
+<option>Dividir</option>
+</select>
+Valor 2:
+<input type="text" name="valor2" size="5" />
+<input type="submit" nome="calcularbtn" value="calcular" />
+</form>
+</BODY>
+</HTML>
