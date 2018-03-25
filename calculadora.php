af0a45069c0c12f5434e24b5d2a988bafade6063
jeovann <37672198+jeovann@users.noreply.github.com>
2018-03-24 21:45:50 -0400

    Update calculadora.php

--- calculadora.php
+++ calculadora.php
@@ -18,3 +18,21 @@ Valor 2:
 </form>
 </BODY>
 </HTML>
+
+<?php
+
+$numero1 = $_POST['numero1'];
+$numero2 = $_POST['numero2'];
+$tipo = $_POST['tipo'];
+
+    switch($tipo)
+    {
+        case 'somar': $resultado = $numero1 + $numero2; break;
+        case 'subtrair': $resultado = $numero1 - $numero2; break;
+        case 'multiplicar': $resultado = $numero1 * $numero2; break;
+        case 'dividir': $resultado = $numero1 / $numero2; break;
+    }
+
+    echo $resultado;
+
+?>
